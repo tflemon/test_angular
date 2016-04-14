a0c1cf47230b4a873d372789e04a6a38def130ce
runMaru1 <fur.tak.m12@outlook.com>
2016-04-14 22:48:57 +0900

    sample1

---
+++ resource.js
@@ -0,0 +1,31 @@
+var myApp = angular.module('mainModule', ['ngResource','ui.bootstrap']);
+
+  myApp.controller('mainController', ['$scope', '$resource',
+    function ($scope, $resource) {
+      $scope.getWithParams = function (withSuccess) {
+        var callURL = (withSuccess ? "resource-server.php" : "invalid-url.php");
+
+        var serverResource = $resource(callURL, {
+          param1: "param1 default",
+          param2: "param2 default"
+        });
+        var getConfig = {};
+        if ($scope.getParam1 !== undefined && $scope.getParam1 != "") {
+          getConfig.param1 = $scope.getParam1;
+        }
+        if ($scope.getParam2 !== undefined && $scope.getParam2 != "") {
+          getConfig.param2 = $scope.getParam2;
+        }
+
+        serverResource.get(getConfig,
+          function (value, responseHeaders) {
+            $scope.getWithParamsResult = "GET SUCCESS\n\n" +
+            "value: " + angular.toJson(value,true) + "\n\n";
+          },
+          function (httpResponse) {
+            $scope.getWithParamsResult = "GET ERROR\n\n";
+          }
+        );
+      };
+    }
+  ]);
